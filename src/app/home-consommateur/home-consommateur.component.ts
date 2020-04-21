0b5dea79d7eb08cefa465373462b41412ec59b7f
ChamsAk <chamsak12@gmail.com>
2020-04-21 20:30:10 +0100

    travail chams

--- src/app/home-consommateur/home-consommateur.component.ts
+++ src/app/home-consommateur/home-consommateur.component.ts
@@ -1,32 +1,134 @@
-import { Component, OnInit } from '@angular/core';
-import { LoginAuthService } from '../services/login-auth.service';
-import { Router } from '@angular/router';
-import { UserService } from '../services/user.service';
-import { ConsommateurModule } from '../models/consommateur/consommateur.module';
+import { Component, OnInit, ɵConsole } from "@angular/core";
+import { LoginAuthService } from "../services/login-auth.service";
+import { Router } from "@angular/router";
+import Quagga from "quagga";
+import $ from "jquery";
+import { ConditionalExpr } from "@angular/compiler";
+
+import { UserService } from "../services/user.service";
+import { ConsommateurModule } from "../models/consommateur/consommateur.module";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { ProductService } from "../services/porduct.service";
+import { throwError } from "rxjs";
 
 @Component({
-  selector: 'app-home-consommateur',
-  templateUrl: './home-consommateur.component.html',
-  styleUrls: ['./home-consommateur.component.css']
+  selector: "app-home-consommateur",
+  templateUrl: "./home-consommateur.component.html",
+  styleUrls: ["./home-consommateur.component.css"],
 })
 export class HomeConsommateurComponent implements OnInit {
-  public loginUser:any={};
-  public user:any={};
-  constructor(private loginAuthService: LoginAuthService,private router: Router,private userService:UserService) {
+  public loginUser: any = {};
+  public user: any = {};
+  public localProducts;
+  barcodeForm: FormGroup;
+  constructor(
+    private productService: ProductService,
+    private formBuilder: FormBuilder,
+    private loginAuthService: LoginAuthService,
+    private router: Router,
+    private userService: UserService
+  ) {
     this.loginAuthService.isLoggedIn();
-    this.loginUser=JSON.parse(localStorage.getItem('currentUser'));
+    this.loginUser = JSON.parse(localStorage.getItem("currentUser"));
+
+    this.barcodeForm = this.formBuilder.group({
+      barcode: ["", [Validators.required]],
+    });
   }
 
-logout(){
-localStorage.removeItem('currentUser');
-this.router.navigate([ '']);
-}
+  logout() {
+    localStorage.removeItem("currentUser");
+    this.router.navigate([""]);
+  }
   ngOnInit(): void {
-//console.log(this.loginUser.token);
-    this.userService.getUser(this.loginUser.token).subscribe(user=>{
-      this.user=user;
-
-    })
+    //console.log(this.loginUser.token);
+    this.userService.getUser(this.loginUser.token).subscribe((user) => {
+      this.user = user;
+    });
+    this.scanner();
   }
 
+  scanner() {
+    if (
+      $("#barcode-scanner").length > 0 &&
+      navigator.mediaDevices &&
+      typeof navigator.mediaDevices.getUserMedia === "function"
+    )
+      // safely access `navigator.mediaDevices.getUserMedia`
+      console.log("hello");
+
+    if (Quagga.initialized == undefined) {
+      Quagga.onDetected(function (result) {
+        var last_code = result.codeResult.code;
+        if (last_code.length == 13) {
+          let last_result = [];
+          last_result.push(last_code);
+          var code = order_by_occurrence(last_result)[0];
+          Quagga.stop();
+          console.log(last_result[0]);
+          document.getElementById("thumbnails").innerHTML = last_result[0];
+        }
+      });
+      var order_by_occurrence = function (arr) {
+        var counts = {};
+        console.log(arr);
+        arr.forEach = function (value) {
+          if (!counts[value]) {
+            counts[value] = 0;
+          }
+          counts[value]++;
+        };
+
+        return Object.keys(counts).sort(
+          (curKey, nextKey) => counts[curKey] - counts[nextKey]
+        );
+      };
+    }
+    Quagga.init(
+      {
+        inputStream: {
+          name: "Live",
+          type: "LiveStream",
+          numOfWorkers: navigator.hardwareConcurrency,
+          target: document.querySelector("#interactive"), // Or '#yourElement' (optional)
+        },
+        decoder: {
+          readers: [
+            "ean_reader",
+            "ean_8_reader",
+            "code_39_reader",
+            "code_39_vin_reader",
+            "codabar_reader",
+            "upc_reader",
+            "upc_e_reader",
+          ],
+        },
+      },
+      function (err) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log("Initialization finished. Ready to start");
+        Quagga.start();
+      }
+    );
+  }
+
+
+
+  listProd() {
+
+    var a=(<HTMLScriptElement>document.getElementById("thumbnails")).textContent;
+      console.log(typeof a);
+      console.log(a);
+
+    this.productService.getLocalProducts(a).subscribe(
+      (res) => (this.localProducts = res),
+      (error) => {
+        console.log("Error");
+        return throwError(error);
+      }
+    );
+  }
 }
--- src/app/services/porduct.service.ts
+++ src/app/services/porduct.service.ts
@@ -1,50 +1,51 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import "rxjs/add/operator/map";
 import { HttpClient, HttpResponse, HttpHeaders } from "@angular/common/http";
-import { Product } from '../models/product';
+import { Product } from "../models/product";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ProductService {
-private produrl='http://localhost:9080/products';
+  private produrl = "http://localhost:9080/products";
   constructor(private http: HttpClient) {
     this.http = http;
   }
 
-
-  createProd(product: Product,token:any): Observable<Product> {
-    const headers = new HttpHeaders({ 'Authorization': 'Bearer ' + token });
-    return this.http.post(this.produrl,product, { headers: headers });
+  createProd(product: Product, token: any): Observable<Product> {
+    const headers = new HttpHeaders({ Authorization: "Bearer " + token });
+    return this.http.post(this.produrl, product, { headers: headers });
   }
 
-
-  updateProd(product: any,token:any): Observable<any> {
+  updateProd(product: any, token: any): Observable<any> {
     const url = `${this.produrl}/${product.id}`;
-    const headers = new HttpHeaders({ 'Authorization': 'Bearer ' + token });
-    return this.http.put(url,product, { headers: headers });
-
+    const headers = new HttpHeaders({ Authorization: "Bearer " + token });
+    return this.http.put(url, product, { headers: headers });
   }
 
   getAllProds(): Observable<any> {
-    const headers = new HttpHeaders({  'Access-Control-Allow-Origin':'*' });
+    const headers = new HttpHeaders({ "Access-Control-Allow-Origin": "*" });
     return this.http.get(this.produrl, { headers: headers });
   }
 
-  getProd(id:number): Observable<any> {
+  getProd(id: number): Observable<any> {
     const url = `${this.produrl}/${id}`;
-    const headers = new HttpHeaders({ 'Access-Control-Allow-Origin':'*' });
+    const headers = new HttpHeaders({ "Access-Control-Allow-Origin": "*" });
     return this.http.get(url, { headers: headers });
   }
 
-  deleteprod(id:number,token:any): Observable<any> {
+  deleteprod(id: number, token: any): Observable<any> {
     const url = `${this.produrl}/${id}`;
-    const headers = new HttpHeaders({ 'Authorization': 'Bearer ' + token });
-    return this.http.delete(url, { headers: headers ,responseType: 'text'});
-
+    const headers = new HttpHeaders({ Authorization: "Bearer " + token });
+    return this.http.delete(url, { headers: headers, responseType: "text" });
   }
 
-
-
+  getLocalProducts(barcode: any): Observable<any> {
+    const url = "http://localhost:9080/products/foreignBarcode";
+    const headers = new HttpHeaders({
+      "Access-Control-Allow-Origin": "*"
+    });
+    return this.http.post(url, barcode, { headers: headers });
+  }
 }
